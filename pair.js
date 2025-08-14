c55123cc08001868e634d5cc0946fdebe77cee7e
ishen1234 <ishenhasaral@gmail.com>
2025-08-14 14:55:52 -0400

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://i.ibb.co/HfPgjbGf/1660.jpg",
               },
               caption: sid,
             });
@@ -129,3 +129,4 @@ process.on("uncaughtException", function (err) {
 });
 
 module.exports = router;
+
